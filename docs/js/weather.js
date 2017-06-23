41ec1aba056d6f5cebeb6746fc8ee42c7716bea0
Alex Reany <reanyalex@gmail.com>
2017-06-22 23:11:40 -0700

    changed api again

--- docs/js/weather.js
+++ docs/js/weather.js
@@ -20,41 +20,46 @@ if (navigator.geolocation) {
 
           // url for local weather based on response of your geolocation needs state and city
           // let locWeatherUrl = "http://dataservice.accuweather.com/currentconditions/v1/" + json.Key + "?apikey=" + apiKey + "&language=en-us&details=true";
-          let locWeatherUrl = "https://api.apixu.com/v1/current.json?key=3abee957470744b8ac851416172306&q="+position.coords.latitude+"%2C" + position.coords.longitude
-          console.log(locWeatherUrl);
+          let locWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=9ab1f88b59caf7d37ca1732508998969&type=accurate"
+
+
+
           fetch(locWeatherUrl).then((res) => res.json())
             .then(function(json) {
               console.log(json);
-
-              // // inserts currentweather icon and description
-              // // document.querySelector(".current-condition-icon").src = currentWeather.icon_url;
-              // document.querySelector(".current-condition-text").innerHTML = json[0].WeatherText
               //
-              // // //inserts current temperature
-              // document.querySelector(".current__temp-text").innerHTML = json[0].Temperature.Imperial.Value + "°F";
+              // // inserts location
+              // document.querySelector(".location").innerHTML = json.location.name + ", " + json.location.region
+              //
+              // // inserts currentweather icon and description
+              // // document.querySelector(".current-condition-icon").src = json.current.condition.icon;
+              // document.querySelector(".current-condition-text").innerHTML = json.current.condition.text;
+              //
+              // // inserts current temperature
+              // document.querySelector(".current__temp-text").innerHTML = json.current.temp_f + "°F";
             })
             // url for local weather based on response of your geolocation needs location code
-//             let weatherForecastUrl =  "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"+json.Key+"?apikey="+apiKey+"&language=en-us&details=true"
+//             let weatherForecastUrl =  "https://api.apixu.com/v1/forecast.json?key=3abee957470744b8ac851416172306&q="+position.coords.latitude+"%2C" + position.coords.longitude+"&days=7"
 //             console.log(weatherForecastUrl);
 //           fetch(weatherForecastUrl).then((res) => res.json())
 //             .then(function(json) {
 //               console.log(json);
-// //               let forecast = json.forecast.simpleforecast.forecastday;
-// //               //inserts todays high and lows
-// //               document.querySelector(".current__temp-value-high").innerHTML = forecast[0].high.fahrenheit + "°F";
-// //               document.querySelector(".current__temp-value-low").innerHTML = forecast[0].low.fahrenheit + "°F";
-// //
-// //               //insert chance of percip and humidity
-// //               document.querySelector(".current-percip").innerHTML = forecast[0].pop + "%"
-// //               document.querySelector(".current-humidity").innerHTML = forecast[0].avehumidity + "%"
-// //
-// //               //creating the div for the forecast
-// //               let forecastDiv = document.querySelector(".forecastDiv")
-// //               for (let i = 1; i <= 7; i++) {
-// //
-// //                 forecastDiv.innerHTML +=
-// //                 '<div class="forecast col-forecast7-md col-3-sm"><div><span class="forecast-date">' +forecast[i].date.weekday + " " + forecast[i].date.month+"/"+forecast[i].date.day+'</span></div><div class="forecast-hl"><span class="forecast-h">'+ forecast[i].high.fahrenheit+'</span><span>'+' | '+'<span class="forecast-l">'+forecast[i].low.fahrenheit+'</span></div><div><img class="forecast-icon" src='+ forecast[i].icon_url +'></div><div><span class="forecast-conditions">'+forecast[i].conditions+'</span></div><div><span class="forecast-pop">'+"☂ "+forecast[i].pop+"%"+'</span></div></div>'
-// //               }
+// // //               let forecast = json.forecast.simpleforecast.forecastday;
+//               //inserts todays high and lows
+//               document.querySelector(".current__temp-value-high").innerHTML = json.forecast.forecastday.maxtemp_f + "°F";
+//               document.querySelector(".current__temp-value-low").innerHTML = json.forecast.forecastday.mintemp_f + "°F";
+// // //
+// // //               //insert chance of percip and humidity
+//               // document.querySelector(".current-percip").innerHTML = json.hour.will_it_rain+ "%"
+//               document.querySelector(".current-humidity").innerHTML = json.humidity + "%"
+// // //
+// // //               //creating the div for the forecast
+// // //               let forecastDiv = document.querySelector(".forecastDiv")
+// // //               for (let i = 1; i <= 7; i++) {
+// // //
+// // //                 forecastDiv.innerHTML +=
+// // //                 '<div class="forecast col-forecast7-md col-3-sm"><div><span class="forecast-date">' +forecast[i].date.weekday + " " + forecast[i].date.month+"/"+forecast[i].date.day+'</span></div><div class="forecast-hl"><span class="forecast-h">'+ forecast[i].high.fahrenheit+'</span><span>'+' | '+'<span class="forecast-l">'+forecast[i].low.fahrenheit+'</span></div><div><img class="forecast-icon" src='+ forecast[i].icon_url +'></div><div><span class="forecast-conditions">'+forecast[i].conditions+'</span></div><div><span class="forecast-pop">'+"☂ "+forecast[i].pop+"%"+'</span></div></div>'
+// // //               }
 //             })
 // //
 //
